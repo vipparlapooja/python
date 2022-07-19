a4f8d37d3ddce46f3752ba7a8a076b6af6a2079f
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-07-19 12:44:08 +0530

    Add files via upload

---
+++ int_float.py
@@ -0,0 +1,59 @@
+''' int and float characters'''
+
+# a=5
+# print(5)
+
+# print(type(0))
+# print(type(2))
+# print(type(032.52))
+# print(type(10))
+# print(type(50))
+
+# print(type(0.4))
+# print(type(.3))
+# print(type(54682.2336))
+# print(type(-562.325))
+
+# a=7
+# print(a)
+# print('%d'         %a)
+# print('                %d'%a)
+# print(%d)
+
+# a=7  ; b=8
+# print(a)
+# print('%d'%b)
+
+
+
+
+# c=6.7
+# # print(c)
+# print('%f' %c)
+
+
+# d=7.82
+# print('%f'%d)
+
+
+# v=4.53
+# print('%.1f'%v)
+# print('%.2f'%v)
+# print('%.3f'%v)
+# print('%.4f'%v)
+# print('%.14f'%v)
+
+''' Type Conversion & Casting'''
+p=73232
+print(type(p))
+print(int(p))
+print(float(p))
+print('%d' %p)
+
+
+# q=5.89
+# print(type(q))
+# print(int(q))
+# print('%d' %q)
+# print('%f'%q)
+
