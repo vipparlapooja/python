e6c172d9153bc4ff1f5ca47c1103c45819764f32
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-07-21 10:48:09 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,44 @@
+# print('hello python')
+# print('''hello python''')
+# print("hello python")
+# print(type('hello world'))
+# print(type("hello world"))
+# print(type('''hello world'''))
+# print(type("""hello world"""))
+
+
+'''working with int,float and str'''
+
+# print(type(6+7.8))
+# print(type(7+2))
+# print(type(2+7.8))
+# print(type(5.2+7.8))
+
+# print('hello','world')
+# print('hello  world')
+# print(1,2,3)
+# print(1  2  3)
+# print('hello'*5)
+# print('hello  '*5)
+ 
+#  print(1,2,3,4,5)
+# print(1,2,3,4,5,sep='')
+# print(1,2,3,4,5)
+# print(1,2,3,4,5,sep=' ')
+# print(1,2,3,4,5,sep='  ')
+# print(1,2,3,4,5,sep="-")
+
+print('hello'+'world')
+print('hello '+'world')
+print('hello'+' world')
+print('hello'+'  world')
+print('hello','world')
+
+
+
+#concatenation
+# print('hello'+''+'world')
+# print('hello'+' '+'world')
+# print('hello'+'  '+'world')
+# print('hello'+'    '+'world')
+# print('hello'+'-'+'world')
