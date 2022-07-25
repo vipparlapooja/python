735c0a4770c7588b72c1fb84d1d158e958fca10f
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-07-25 11:47:36 +0530

    Create string_2.py

---
+++ string_2.py
@@ -0,0 +1,77 @@
+# a=2
+# b=6.7
+# c='hi'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# a='hi' ;  b='hey'
+# print(a+b)
+# print(a,b)
+# print(a+' '+b)
+# print(a+'   '+b)
+
+# a='hi'  ; b='hey' ; c=5
+
+# print(a+str(c))
+# print(a+str(d))
+# print(a+'3.10')
+
+# a=5 ; b=7.8 ; c='hi'
+# # print(int(b)) # 7
+# # print(float(a)) # 5.0
+# print(int(c))
+# print(float(c))
+
+# x='6'
+# print(type(x))
+# print(int(x)) # 6
+# print(float(x)) # 6.0
+
+# y='7.8'
+# print(type(y))
+# print(float(y))
+# a=float(y)  # a=7.8
+# print(int(a)) # 7
+
+# print('hi')
+# print('bye')
+# print('Python') ; print('Java')
+# print('h' , 'i')
+# print('h'  'i')
+# print('hi'
+# 'bye')
+# print('hi'
+
+# 'hello')
+# print('hi'                   'bye')
+# print('hi '                   'bye')
+# print('hi how are you , what are \
+# you doing')
+
+print('''how
+are 
+you?''')
+
+print('''how
+   are 
+you?''')
+
+print("""what's going on?""")
+
+print('today is thur\'sday')
+
+# print('hi')
+# print()
+# print('hello')
+
+# print('6.7'  * 3)
+
+# print('hello' * 4.5)
+# print('hello' * 'python')
+
+# print(''+'hi')
+# print(' '+'hi')
+# print('','hi')
+# print(' ','hi')
+ 
