caa39d7e74ae7ba57cd6112f906d6e9e6c84bc91
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-07-27 12:04:41 +0530

    Create ways_to_assign_variables.py

---
+++ ways_to_assign_variables.py
@@ -0,0 +1,40 @@
+
+# a=1
+# b=2
+# c=3
+# d=4
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e) =Name Error
+
+# '''Second Way'''
+# a=1; b=2 ; c=3
+# print(a)
+# print(b)
+# print(c)
+
+# """Third way"""
+# a=1; b=2 ; c=3
+# print(a) ;print(b) ; print(c)
+
+""" Fourth Way"""
+
+a=2,3,4,6
+# print(type(a))  *answer is tuple
+print(a)
+print(a,type(a))
+
+b=[5,6,7,8]
+print(b)
+print(b,type(b))
+# print(b,type(b))  *answer is list
+
+# a=1; b=2; c=3
+# print(a)
+# print(b)
+# print(c)
+
+a,b,c,d=2,3,4,6
+print(a) ; print(b) ; print(c) ; print(d)
