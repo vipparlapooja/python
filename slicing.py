0b00e124976fd4b69f53439616703b3910910f01
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-07-28 22:48:47 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,58 @@
+# c='python'
+# print(c[4])
+# print(c[1])
+# print(c[5])
+# print(c[2])
+# print(c[6])
+
+# c='python'
+# print(c[0:])
+# print(c[1:])
+# print(c[2:])
+# print(c[3:])
+# print(c[4:])
+# print(c[5:])
+
+# w='python'
+# print(w[:])
+# print(w[4:])
+# print(w[:3])
+# print(w[2:5])
+# print(w[4:1])
+
+# k='python'
+# print(k[-1])
+# print(k[-2])
+# print(k[-3])
+# print(k[-4])
+# print(k[-5])
+# print(k[-6])
+
+# g='python'
+# print(g[0]+g[-1])
+
+""" program to find the sum of the first and last digit  """
+
+# a='5647551234558458'
+# print(type(a)) # str
+# print(a[0]) # 1
+# print(a[-1]) # 5
+# print(a[0]+a[-1]) # 15
+# print(int(a[0])+int(a[-1]))
+
+
+# f='python'
+# print(f[2:7])
+# print(f[1:-2])
+# print(f[-4:0])
+# print(f[-1:1])
+
+""" Reverse of a string """
+# n='python'
+# print(n[::-1])
+
+
+c='python'
+print(c[::])
+print(c[0::1])
+print(c[0::2])
