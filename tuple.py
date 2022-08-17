5ab53ce8c24d3f20851941e446f6c58af27f8b76
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-08-17 23:00:08 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# x=(1)
+# print(x,type(x))
+
+# x=(1,)
+# print(x,type(x))
+
+# # pr=('apple','reliance','asus','dell','samsung')
+# # pr[1]="Motorolla"
+
+# # n="Hello" ,  "World"
+# # print(n,type(n)) 
+# # print(n[2])
+
+# # v=(1,4.2,'hello',[2,3],('H','D'),None,True)
+# # print(v,type(v)) 
+
+# # v=(1,4.2,'hello',[2,3],('H','D'))
+# # v[0]=21
+
+# v=(1,4.2,'hello',[2,7],('H','D'))
+# print(v.count(4.2))
+# print(v.index([2,7]))
+
+
+print(dir(tuple))
