430bbf5d53153d07ee02e4bbf9ac469d7e5f9668
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-08-21 18:47:59 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,76 @@
+# a=input('Enter your Name:')
+# print("My Name is",a)
+# print("My Name is",a,sep="-")
+# print("My Name is",int(a))  
+# print("My Name is",float(a)) 
+# print("My Name is"+' '+str(a))
+# print("My Name is"+' '+a)
+
+# a=int(input('Enter your Phone Number:'))
+# print("Your Phone Number is",a)
+# print("Your Phone Number is",a,sep=" ")
+# print("Your Phone Number is",int(a))
+# print("Your Phone Number is",float(a))
+# print("Your Phone Number is"+' '+str(a))
+
+# a=float(input('Enter your BMI:'))
+# print("Your BMI is",a)
+# print("Your BMI is",a,sep=" ")
+# print("Your BMI is",int(a))
+# print("Your BMI is",float(a))
+# print("Your BMI is"+' '+str(a))
+
+# name=input("Enter Your Name:")
+# age=int(input("Enter Your Age:"))
+# per=float(input("Enter your Graduation Percentage:"))
+
+# print(name,age,per)
+
+# print(age,name,per)
+
+# print(name,age,name)
+
+# print(name)
+
+# name=input("Enter Your Name:")
+# age=int(input("Enter Your Age:"))
+# per=float(input("Enter your Graduation Percentage:"))
+
+# print("Your Name is",name,"and age is",age,\
+# "Graduation percentage is",per)
+
+# print("Your Name is"+' '+name,"and age is",age, \
+# "Graduation percentage is",str(per)+'%')
+
+# print("Your Name is %s and age is %d. \
+# Graduation percentage is %.2f"%(name,age,per))
+
+# print("Your Name is {} and age is {}. \
+# Graduation percentage is {}".format(name,age,per))
+
+# print("Your Name is {1} and age is {0}. \
+#  Graduation percentage is {2}".format(age,name,per))
+
+# print(f"Your Name is {name} and age is {age}.\
+# Graduation percentage is {per}%")
+
+
+# a=int(input())
+# b=int(input())
+# print(a+b)
+
+# a,b=[int(x) for x in input('Enter any two Numbers: ').split()]
+# print(a+b)
+
+# a=[float(x) for x in input().split(',')]
+# print(sum(a))
+
+# import math
+# a=[float(x) for x in input().split(',')]
+# print(math.prod(a))
+
+x='1234335346'
+print(int(x[0])+int(x[-1]))
+
+x=input()
+print(int(x[0])+int(x[-1]))
