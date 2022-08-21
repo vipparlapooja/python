e1b0bc1e31aab6fcfc14feeaf07023252e33a8d1
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-08-21 19:10:21 +0530

    Add files via upload

---
+++ end_sep.py
@@ -0,0 +1,65 @@
+# print(1,2,3,4,5)
+# print('pyt' 'hon')  
+# print('pyt' ,'hon')  
+# print('pro' + str(3))
+# print('Poco' 'M4' 'Pro')
+# print('Poco' ,'M4' ,'Pro')
+# print('Poco'+''+'M4'+''+'Pro')
+# print('Poco'+' '+'M4'+' '+'Pro')
+# print('Poco'+'  '+'M4'+'  '+'Pro')
+
+# print(1,2,3,4,5)
+# print('1'+ '2' +'3'+ '4'+ '5')
+# print('1'+''+'2'+''+'3'+''+'4'+''+'5')
+# print('1'+' '+'2'+' '+'3'+' '+'4'+' '+'5')
+
+# print(1,2,3,4,5,sep='')
+# print('h' 'i',sep=' ')
+# print('h' , 'i',sep=' ')
+# print('h' , 'i',sep='-')
+
+# print(10,20,30,40,sep='-')
+# print(10,20,30,40,sep='*')
+# print(10,20,30,40,sep='@')
+
+# print('python',end='')
+# print('program')
+
+# print('python',end='')
+# print('program')
+# print('code')
+
+# print('python',end=' ')
+# print('program')
+
+# print('python',end='.')
+# print('program')
+
+# print('python',end='. ')
+# print('program')
+
+# print('python',end='')
+# print('code',end='')
+# print('programmer')
+
+# print('python',end=' ')
+# print('code',end=' ')
+# print('programmer')
+
+# print(1,2,3,4,end=' ',sep='-')
+# print(5,6,7,8,sep=' ',end='.')
+# print(9,10)
+
+# print('python\n''program')
+# print('python''\nprogram')
+# print('python\n''\nprogram')
+
+# print('hello' * 3)
+# print('hello ' * 3)
+# print('hello\n' *3)
+
+print('hello\n'*3,end='.')
+print('hello\n'*3,end='')
+
+print('hello',end='\n')
+print('code')
