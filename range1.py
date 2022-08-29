5095f9277399f54fd607d71c051ab588c09bcfe5
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-08-29 20:08:52 +0530

    Add files via upload

---
+++ range1.py
@@ -0,0 +1,36 @@
+# a=1,2,3,4,5
+# print(*a)
+
+# a=range(10)
+# print(a)
+
+# a=10,20,40
+# for j in a:
+#     print(j,end=' ')
+
+# for i in range(5):
+#     print(i)
+
+# n=int(input("Enter the value of n:"))
+# for i in range(1,n+1):
+#     print(i,end="\n")
+
+# for i in range(1,30,2):
+#     print(i,end=' ')
+
+# for i in range(2,30,2):
+#     print(i,end=' ')
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(10,-1,-1):
+#     print(i)
+
+# n=[1,4.5,'python']
+# for i in n[::-1]:
+#     print(i)
+
+# v='programming'
+# for e in v[::2]:
+#     print(e)
