172fe15298a20b9d1207db80b3f867d098bb1c53
vipparlapooja <109496991+vipparlapooja@users.noreply.github.com>
2022-08-29 20:08:33 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,25 @@
+# a=range(5)
+# print(a,type(a))
+
+# b=range(5)
+# for pyt in b:
+#     print(pyt)
+
+# for i in range(7):
+#     print(i)
+
+# for i in range(7):
+#     print(i,end=' ')
+
+# for i in range(1,9):
+#     print(i)
+
+# for i in range(1,9,2):
+#     print(i)
+
+# for j in range(2,51,2):
+#     print(j)
+
+
+
+
